ac258962ec7b0e33c824fa7d435323386129e561
Robin Swamidasan <robin_swamidasan@yahoo.com>
2015-01-26 03:13:19 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,95 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##----------------------------------------------------------------------------
+##
+##              Matrix Inverter and Cache
+##
+##   The functions below are used in conjunction to find the  
+##   inverse of a matrix and cache it for future retrieval. 
+##
+##   It is operated as follows:
+##
+##      1. The first time around makeCacheMatrix is called
+##         with the fresh matrix as the argument. 
+##         (makeCacheMatrix does not need to be called again
+##          for the same matrix.)
+##      2. The value returned by makeCacheMatrix is used as
+##         the argument to cacheSolve
+##      3. For a given matrix, the first time cacheSolve is called,
+##         it retrieves the matrix from the cache, calculates its
+##         Inverse, stores the Inverse in the cache, and returns
+##         the Inverse to the caller.
+##      4. In subsequent calls to cacheSolve, the Inverse is 
+##         retrieved from the cache and returned to the caller.
+##
+## ----------------------------------------------------------------------------
+##
+##  The makeCacheMatrix function does the following:
+##
+##     1. Initializes the Inverse cache, mI
+##
+##     2. Stores the Matrix on the first call to this funtion.
+##    
+##     3. Returns a list of functions that are used by cacheSolve to:
+##
+##        + Retrieve the matrix (getMat)
+##        + Store the Inverse in the cache (setInv)
+##        + Retrieve the Inverse from the cache (getInv)
+##
+## --------------------------------------------------------------------------------
 
-makeCacheMatrix <- function(x = matrix()) {
-  mI <- NULL
-  set <- function(y) {
-    x <<- y
-    mI <<- NULL
-  }
-  get <- function() x
-  setInv <- function(solve) mI <<- solve
-  getInv <- function() mI
-  list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
+makeCacheMatrix <- function(mat = matrix()) {
+  
+  mI <- NULL          ##  Initialize the Inverse cache
+  
+  getMat <- function() mat             ##  Store (1st Time) and 
+                                       ##  Retrieve the matrix
+  
+  setInv <- function(Inv) mI <<- Inv   ##  Store the Inverse
+  
+  getInv <- function() mI              ##  Retrieve the Inverse
+    
+  list(getMat = getMat,       ##  return a list of functions
+       setInv = setInv,       ##  that will be called by
+       getInv = getInv)       ##  cacheSOlve
 }
 
 
-## Write a short comment describing this function
+## --------------------------------------------------------------------------------
+## 
+##   The cacheSolve function does the following:
+##
+##       1. Checks the cache to see if the Inverse has already been 
+##          and stored.
+##       2. If the Inverse is present in the cache it is retrieved
+##          and returned to the caller.
+##       3. If not, the matrix is retrieved from the cache and its Inverse
+##          is calculated.
+##       4. The Inverse is stored in the cache and returned to the caller.
+##
+## ---------------------------------------------------------------------------
 
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  mI <- x$getInv()
-  if(!is.null(mI)) {
-    message("getting cached Inverse Matrix")
-    return(mI)
+cacheSolve <- function(xlist) {
+  
+  mI <- xlist$getInv()
+  
+  if(!is.null(mI)){
+    message("Retrieving Inverse from Cache")
+    
+    return(mI)               ## Return Inverse to caller
   }
-  data <- x$get()
-  mI <- solve(data, ...)
-  x$setInv(mI)
-  mI
+  else {
+    message("Calculating Inverse ... and Storing it")
+    
+    data <- xlist$getMat()   ## Retrieve matrix from cache
+    
+    mI <- solve(data)        ## Calculate Inverse
+    
+    xlist$setInv(mI)         ## Store Inverse in Cache
+    
+    return(mI)               ## Return Inverse to caller
+  }
+  
 }
+
+##---------------------------------------------------------------------------
+
